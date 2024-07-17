0a5da250add07df91ccdef9ff8236badb6f491f8
NaveedUrRehman787 <naveedurrehman787@gmail.com>
2024-07-17 05:44:11 +0500

    Logger updated

--- src/logger.py
+++ src/logger.py
@@ -3,19 +3,18 @@ import os
 from datetime import datetime
 
 
-def my_logger():
-    LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-    logs_path = os.path.join(os.getcwd(),
-                            'logs',
-                            LOG_FILE
-                            )
-    os.makedirs(logs_path,exist_ok=True)
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path = os.path.join(os.getcwd(),
+                        'logs',
+                        LOG_FILE
+                        )
+os.makedirs(logs_path,exist_ok=True)
 
-    LOG_FILE_PATH = os.path.join(logs_path,LOG_FILE)
+LOG_FILE_PATH = os.path.join(logs_path,LOG_FILE)
 
-    logging.basicConfig(
-        filename=LOG_FILE_PATH,
-        format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
-        level=logging.INFO
-    )
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO
+)
 
